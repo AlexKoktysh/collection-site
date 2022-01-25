4b17d767b777d2f5ff9f1d0328eff908b1c6550c
Mr-Prezident-Sasha <awot0058@gmail.com>
2022-01-25 16:00:16 +0300

    build

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -8,6 +9,15 @@ app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(_dirname, '..', 'frontend', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = config.get('port') || 5000
 
